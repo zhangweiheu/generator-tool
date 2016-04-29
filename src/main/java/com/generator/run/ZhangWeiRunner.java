5da5675d9914fe3e4b36cf0c12fcfe57f4f778a2
zhangwei <zhangweiheu@gmail.com>
2016-04-29 18:48:54 +0800

    ssh

--- src/main/java/com/generator/run/ZhangWeiRunner.java
+++ src/main/java/com/generator/run/ZhangWeiRunner.java
@@ -10,24 +10,16 @@ public class ZhangWeiRunner {
         Runner runner = new Runner();
         runner.setConfigFile(ZhangWeiRunner.class.getClassLoader().getResource("graduation_generatorConfig.xml").getFile());
         runner.setContextIds("context1");
-//        runner.setTargetProject("D:\\GraduationProject\\exams-system-core\\src\\main\\java");
-        runner.setTargetProject("D:\\GraduationProject\\webapp\\src\\main\\java");
-//        runner.setJavaModelTargetPackage("com.online.exams.system.core.model");
-        runner.setJavaModelTargetPackage("com.shop.model");
-//        String mapperPackage = "com.online.exams.system.core.mapper";
-        String mapperPackage = "com.shop.mapper";
+        runner.setTargetProject("D:\\GraduationProject\\SSH\\src\\main\\java");
+        runner.setJavaModelTargetPackage("model");
+        String mapperPackage = "mapper";
         runner.setSqlMapTargetPackage(mapperPackage);
         runner.setJavaClientTargetPackage(mapperPackage);
 
-        //        设置生成的表
-//        runner.addFullyQualifiedTable("question");
-//        runner.addFullyQualifiedTable("tag");
-//        runner.addFullyQualifiedTable("paper");
-//        runner.addFullyQualifiedTable("user");
         runner.addFullyQualifiedTable("user");
-        runner.addFullyQualifiedTable("goods");
-        runner.addFullyQualifiedTable("order");
-        runner.addFullyQualifiedTable("order_detail");
+        runner.addFullyQualifiedTable("activity");
+        runner.addFullyQualifiedTable("news");
+        runner.addFullyQualifiedTable("sign_activity");
         runner.run();
     }
 }
